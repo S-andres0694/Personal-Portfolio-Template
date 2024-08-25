e846496324bb64edae418862a3861c981da9b2df
S-andres0694 <sebasandres0694@gmail.com>
2024-08-25 16:26:16 +0100

    feat: Update CSS file paths in head.html
    - fixed the serve issue on the jekyll serve

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./_layouts/portfolio.html'],
+  content: ['./_layouts/portfolio.html', './_site/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
